12f859dc68bd18e734dfbf39e8457945bbf00cb1
jeff-hager-dev <jeffrey.hager.dev@gmail.com>
2016-10-22 11:54:02 -0500

    updating the questions

--- phone/questions.js
+++ phone/questions.js
@@ -6,5 +6,30 @@ module.exports = {
      {"option": "transport(2, transport)", "nextState": "Transportation"},
      {"option": "shelter(3, shelter)",  "nextState": "Shelter"}
    ]
- }
+ },
+  "Food": {
+    "Text": "These are your food options",
+    "Choices": [
+      {"option": "Free(1, food)", "nextState": "End"},
+      {"option": "Discount(2, transport)", "nextState": "End"}
+    ]
+  },
+  "Transport": {
+    "Text": "Do you need help with public transportation",
+    "Choices": [
+      {"option": "Free(1, food)", "nextState": "End"},
+      {"option": "Discount(2, transport)", "nextState": "End"}
+    ]
+  },
+  "Shelter": {
+    "Text": " What kind of help do you need for shelters",
+    "Choices": [
+      {"option": "Free(1, food)", "nextState": "End"},
+      {"option": "Discount(2, transport)", "nextState": "End"}
+    ]
+  },
+  "End": {
+    "Text": "Thank you for calling the Service St.Louis Help line",
+    "Choices": []
+  }
 };
