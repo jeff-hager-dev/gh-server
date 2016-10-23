e5a9f01fd71a1f9d91d0474ca4842ed0b770f921
jeff-hager-dev <jeffrey.hager.dev@gmail.com>
2016-10-22 19:20:13 -0500

    removing an unneeded value

--- phone/questions.js
+++ phone/questions.js
@@ -52,7 +52,6 @@ module.exports = [
       "Text": " You need help with Housing Assistance what would you like to know",
       "nextState": "call",
       "Choices": [
-          {"option": 1, "nextState": "Call", "phoneNumber": "6365786943 "},
       ]
     },
     {
